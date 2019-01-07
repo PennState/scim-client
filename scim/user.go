64155a3d3090929c8bbb86aa31daaacddff77a4c
Steve Moyer <smoyer@psu.edu>
2019-01-07 09:34:58 -0500

    Adds missing field tags to all attributes

--- scim/user.go
+++ scim/user.go
@@ -1,49 +1,49 @@
 package scim
 
 //User describes a SCIM user based on the RFC7643 specification
+//https://tools.ietf.org/html/rfc7643#section-4.1
 type User struct {
-
 	ScimResource
 
 	//Active informs as to whether this User record is currently live in the system
 	Active bool `json:"active"`
 
 	//Addresses is the  physical mailing address for this User, as described in (address Element). Canonical Type Values of work, home, and other. The value attribute is a complex type with the following sub-attributes.
-	Addresses []Address
+	Addresses []Address `json:"addresses"`
 
 	//DisplayName is the name of the User, suitable for display to end-users. The name SHOULD be the full name of the User being described if known
-	DisplayName string
+	DisplayName string `json:"displayName"`
 
 	//Emails are E-mail addresses for the user. The value SHOULD be canonicalized by the Service Provider, e.g. bjensen@example.com instead of bjensen@EXAMPLE.COM. Canonical Type values of work, home, and other.
-	Emails []Email
+	Emails []Email `json:"emails"`
 
 	//Entitlements is a collection of entitlements
-	Entitlements []Entitlement
+	Entitlements []Entitlement `json:"entitlements"`
 
 	//Groups is a list of groups that the user belongs to, either thorough direct membership, nested groups, or dynamically calculated")
 	Groups []Group `json:"groups"`
 
 	//Ims are instant messaging addresses for the User.
-	Ims []Im
+	Ims []Im `json:"ims"`
 
 	//Locale is used to indicate the User's default location for purposes of localizing items such as currency, date time format, numerical representations, etc.
 	Locale string `json:"locale"`
 
 	//Name is the components of the user's real name. Providers MAY return just the full name as a single string in the formatted sub-attribute, or they MAY return just the individual component attributes using the other sub-attributes, or they MAY return both. If both variants are returned, they SHOULD be describing the same name, with the formatted name indicating how the component attributes should be combined.
-	Name Name
+	Name Name `json:"name"`
 
 	//NickName is the casual way to address the user in real life, e.g.'Bob' or 'Bobby' instead of 'Robert'. This attribute SHOULD NOT be used to represent a User's username (e.g. bjensen or mpepperidge)
 	NickName string `json:"nickName"`
 
-	//Password ishe User's clear text password.  This attribute is intended to be used as a means to specify an initial password when creating a new User or to reset an existing User's password.
+	//Password is the User's clear text password.  This attribute is intended to be used as a means to specify an initial password when creating a new User or to reset an existing User's password.
 	//(This is a placeholder in case I implement the server in golang)
 	Password string `json:"_,omitempty"`
 
 	//PhoneNumberrs are the phone numbers for the User.  The value SHOULD be canonicalized by the Service Provider according to format in RFC3966 e.g. 'tel:+1-201-555-0123'.  Canonical Type values of work, home, mobile, fax, pager and other.")
-	PhoneNumbers []PhoneNumber
+	PhoneNumbers []PhoneNumber `json:"phoneNumbers"`
 
 	//Photos are URLs of photos of the User.
-	Photos []Photo
+	Photos []Photo `json:"photos"`
 
 	//Profile URL is a fully qualified URL to a page representing the User's online profile
 	ProfileURL string `json:"profileUrl"`
@@ -52,7 +52,7 @@ type User struct {
 	PreferredLanguage string `json:"preferredLanguage"`
 
 	//Rols are a list of roles for the User that collectively represent who the User is; e.g., 'Student', 'Faculty'.
-	Roles []Role
+	Roles []Role `json:"roles"`
 
 	//Timezone is the User's time zone in the 'Olson' timezone database format; e.g.,'America/Los_Angeles'
 	Timezone string `json:"timezone"`
@@ -67,7 +67,7 @@ type User struct {
 	UserType string `json:"userType"`
 
 	//X509Certificates is list of certificates issued to the User.
-	X509Certificates []X509Certificate
+	X509Certificates []X509Certificate `json:"x509Certificates"`
 }
 
 //Address is a street and country based addess for the identity
@@ -84,13 +84,13 @@ type Address struct {
 	Locality string `json:"locality"`
 
 	//PostalCode is the zipcode or postal code component.
-	PostalCode string
+	PostalCode string `json:"postalCode"`
 
 	//Region is the state or region component.
-	Region string
+	Region string `json:"region"`
 
 	//StreetAddress is the full street address component, which may include house number, street name, PO BOX, and multi-line extended street address information. This attribute MAY contain newlines.")
-	StreetAddress string
+	StreetAddress string `json:"streetAddress"`
 }
 
 type Email = StringMultivalued
