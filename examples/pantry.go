6b653e51ff9dedcd1119ca42dd1b6d6de7f3b978
Steve Moyer <smoyer@psu.edu>
2019-01-11 15:22:13 -0500

    Provides a (tested) custom SCIM extension for the README

---
+++ examples/pantry.go
@@ -0,0 +1,15 @@
+package examples
+
+//Pantry is a SCIM Extension that allows employees and employers to track
+//the amount owed to the pantry (or to the employee).
+type Pantry struct {
+	Building string  `json:"building"` //Building is the location housing the employee's office
+	Office   string  `json:"office"`   //Office is the room number of the employee's office
+	Balance  float64 `json:"balance"`  //Balance is the amount the employee owes to the pantry (if negative).  Credits can be represented by positive Balance values
+}
+
+//GetURN returns the SCIM Extension's URN (identifier) and, more importantly
+//identifies the Pantry struct as a SCIM extension.
+func (p Pantry) GetURN() string {
+	return "urn:com:example:2.0:Pantry"
+}
---
+++ examples/pantry_test.go
@@ -0,0 +1,49 @@
+package examples
+
+import (
+	"testing"
+
+	"github.com/PennState/golang_scimclient/scim"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const pantryJSON = `
+{
+	"schemas": [
+		"urn:ietf:params:scim:schemas:core:2.0:User",
+		"urn:com:example:2.0:Pantry"
+	],
+	"id": "2819c223-7f76-453a-919d-413861904646",
+	"externalId": "701984",
+	"userName": "bjensen@example.com",
+	"urn:com:example:2.0:Pantry": {
+		"building": "Technology Support Building",
+		"office": "202BB",
+		"balance": 4.25
+	},
+	"meta": {
+		"resourceType": "User",
+		"created": "2010-01-23T04:56:22Z",
+		"lastModified": "2011-05-13T04:42:34Z",
+		"version": "W/a330bc54f0671c9",
+		"location": "https://example.com/v2/Users/2819c223-7f76-453a-919d-413861904646"
+	}
+}`
+
+func TestPantryExtension(t *testing.T) {
+	assert := assert.New(t)
+	require := require.New(t)
+
+	var user scim.User
+	err := scim.Unmarshal([]byte(pantryJSON), &user)
+	require.Nil(err)
+
+	var pantry Pantry
+	require.True(user.HasExtension(&pantry))
+	err = user.GetExtension(&pantry)
+	assert.Nil(err)
+	assert.Equal("Technology Support Building", pantry.Building)
+	assert.Equal("202BB", pantry.Office)
+	assert.Equal(4.25, pantry.Balance)
+}
