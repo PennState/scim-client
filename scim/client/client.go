f21a987dd630bb1ea4b071569f3856574542308f
Steve Moyer <smoyer@psu.edu>
2019-01-15 16:06:46 -0500

    Stub of the client code ... needs HTTP verbs for SCIM resources

--- scim/client/client.go
+++ scim/client/client.go
@@ -1,26 +1,56 @@
 package client
 
 import (
+	"io/ioutil"
 	"net/http"
 	"net/url"
 
 	"github.com/PennState/golang_scimclient/scim"
+	log "github.com/sirupsen/logrus"
 )
 
 type Client struct {
-	server *url.URL
-	client *http.Client
+	server       *url.URL
+	client       *http.Client
+	resourceType scim.ResourceType
 }
 
-func NewClient(svr string, client *http.Client) (*Client, error) {
-	_, err := url.Parse(svr)
+func New(scimServerUrl string, httpClient *http.Client) (Client, error) {
+	var scimClient Client
+	var err error
+
+	scimClient.server, err = url.Parse(scimServerUrl)
 	if err != nil {
-		return nil, err
+		return scimClient, err
 	}
 
-	return nil, err
+	scimClient.client = httpClient
+
+	return scimClient, err
 }
 
-func (c Client) Get(resourceType string, id string) *scim.Resource {
-	return nil
+func (c Client) ForEndpoint(endPoint string) (Client, error) {
+	var rt scim.ResourceType
+	rt.Endpoint = endPoint
+	return c.ForResourceType(rt)
+}
+
+func (c Client) ForResourceType(resourceType scim.ResourceType) (Client, error) {
+	return c, nil
+}
+
+func (c Client) Get(path string, user *scim.User) error {
+	resp, err := c.client.Get(c.server.String() + path)
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	log.Infof("Body: %s", body)
+
+	return scim.Unmarshal(body, user)
 }
