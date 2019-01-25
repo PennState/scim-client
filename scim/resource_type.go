70b3e7a2ea3f7164440166b9d0bd78a935eef255
Steve Moyer <smoyer@psu.edu>
2019-01-25 14:33:36 -0500

    Adds comments and makes all fields public (for JSON unmarshaling)

--- scim/resource_type.go
+++ scim/resource_type.go
@@ -1,51 +1,45 @@
 package scim
 
+//ResourceTypeURN is the SCIM URN registered to identify the ResourceType
+//data structure.
+//https://tools.ietf.org/html/rfc7643#section-6
 const ResourceTypeURN = "urn:ietf:params:scim:schemas:core:2.0:ResourceType"
 
-type ResourceType interface {
-	NewResourceType() resourceType
-	NewResource() Resource
+//ResourceType specifies the metadata about a SCIM resource type.
+//https://tools.ietf.org/html/rfc7643#section-6
+type ResourceType struct {
+	CommonAttributes
+	Name             string            `json:"name" validate:"required`
+	Description      string            `json:"description"`
+	Endpoint         string            `json:"endpoint"`
+	Schema           string            `json:"schema"`
+	SchemaExtensions []SchemaExtension `json:"schemaExtensions"`
 }
 
-type resourceType struct {
-	id               string            `json:"id" validate:"required"`
-	schemas          []string          `json:"schemas" validate:"required"`
-	name             string            `json:"name" validate:"required`
-	description      string            `json:"description"`
-	endpoint         string            `json:"endpoint"`
-	schema           string            `json:"schema"`
-	schemaExtensions []schemaExtension `json:"schemaExtensions"`
+//SchemaExtension is a list of URIs that specify the allowed
+//SCIM extensions for a ResourceType
+type SchemaExtension struct {
+	Schema   string `json:"schema"`
+	Required bool   `json:"required"`
 }
 
-func (rt resourceType) ID() string                          { return rt.id }
-func (rt resourceType) Schemas() []string                   { return rt.schemas }
-func (rt resourceType) Name() string                        { return rt.name }
-func (rt resourceType) Description() string                 { return rt.description }
-func (rt resourceType) Endpoint() string                    { return rt.endpoint }
-func (rt resourceType) Schema() string                      { return rt.schema }
-func (rt resourceType) SchemaExtensions() []schemaExtension { return rt.schemaExtensions }
-
-type schemaExtension struct {
-	schema   string `json:"schema"`
-	required bool   `json:"required"`
+//ResourceTypeResourceType is the constant data that defines the metadata
+//for the ResourceType server discovery endpoint
+var ResourceTypeResourceType = ResourceType{
+	CommonAttributes: CommonAttributes{
+		Schemas: []string{
+			ResourceTypeURN,
+		},
+		ID: "ResourceType",
+	},
+	Name:        "ResourceType",
+	Endpoint:    "/ResourceTypes",
+	Description: "SCIM ResourceType - See https://tools.ietf.org/html/rfc7643#section-6",
+	Schema:      ResourceTypeURN,
 }
 
-func NewSchemaExtension(schema string, required bool) schemaExtension {
-	return schemaExtension{
-		schema:   schema,
-		required: required,
-	}
+//ServerDiscoveryResourceType allows a ResourceType to retrieve the related
+//metadata for its SCIM type.
+func (rt ResourceType) ServerDiscoveryResourceType() ResourceType {
+	return ResourceTypeResourceType
 }
-
-func (se schemaExtension) Schema() string { return se.schema }
-func (se schemaExtension) Required() bool { return se.required }
-
-// var UserResourceType = NewResourceType{
-// 	CommonAttributes: CommonAttributes{
-// 		ID:      "User",
-// 		Schemas: []string{"urn:"},
-// 	},
-// 	Name:        "User",
-// 	Description: "",
-// 	Endpoint:    "/Users",
-// }
