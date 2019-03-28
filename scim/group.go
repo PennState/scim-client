f178fe760384496bd2d35f415e51ae0cc37ced90
Steve Moyer <smoyer@psu.edu>
2019-03-28 09:05:36 -0400

    Makes Group a ResourceType

--- scim/group.go
+++ scim/group.go
@@ -11,3 +11,36 @@ type Group struct {
 //MemberRef provides a reference (URI) to a group member as well as a small
 //amount of cargo data.
 type MemberRef StringMultivalued
+
+//GroupURN is the IANA registered SCIM name for the standardized SCIM
+//Group.
+const GroupURN = "urn:ietf:params:scim:schemas:core:2.0:Group"
+
+//GroupResourceType provides the default structure which connects the User
+//struct to its associated ResourceType.
+var GroupResourceType = ResourceType{
+	CommonAttributes: CommonAttributes{
+		Schemas: []string{
+			ResourceTypeURN,
+		},
+		ID: "Group",
+	},
+	Name:        "Group",
+	Endpoint:    "/Groupss",
+	Description: "SCIM ResourceType - See https://tools.ietf.org/html/rfc7643#section-6",
+	Schema:      ResourceTypeURN,
+}
+
+//URN returns the IANA registered SCIM name for the User data structure
+//and, together with ResourceType() identifies this code as implementing
+//the Resource interface.
+func (g Group) URN() string {
+	return GroupURN
+}
+
+//ResourceType returns the default structure describing the availability
+//of the User resource and, together with URN() identifies this code as
+//implementing the Resource interface.
+func (g Group) ResourceType() ResourceType {
+	return GroupResourceType
+}
