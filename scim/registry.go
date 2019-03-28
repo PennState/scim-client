2a034b2d2be51956b5c2a1c521f81c24ecb8b6b4
Steve Moyer <smoyer@psu.edu>
2019-03-28 10:11:51 -0400

    Documents the exported features

--- scim/registry.go
+++ scim/registry.go
@@ -2,6 +2,7 @@ package scim
 
 import "sync"
 
+//ResourceRegistry contains a map which will be treated as a singleton.
 type ResourceRegistry struct {
 	resourceMap map[string]ResourceType
 }
@@ -9,6 +10,10 @@ type ResourceRegistry struct {
 var instance *ResourceRegistry
 var once sync.Once
 
+//GetResourceRegistry returns creates (if necessary) and returns the
+//singleton instance of the ResourceRegistry.  The five ResourceTypes
+//defined in the SCIM specifications are included in the registry by
+//default.
 func GetResourceRegistry() *ResourceRegistry {
 	once.Do(func() {
 		instance = &ResourceRegistry{
@@ -25,11 +30,16 @@ func GetResourceRegistry() *ResourceRegistry {
 	return instance
 }
 
+//Lookup returns the ResourceType associated with the name parameter
+//as well as a boolean that indicates whether the registry contained
+//the requested ResourceType.
 func (rr ResourceRegistry) Lookup(name string) (ResourceType, bool) {
 	rt, ok := rr.resourceMap[name]
 	return rt, ok
 }
 
+//Register allows one or more custom ResourceTypes to be added to the
+//registry.
 func (rr ResourceRegistry) Register(rts ...ResourceType) {
 	for _, rt := range rts {
 		rr.resourceMap[rt.Name] = rt
