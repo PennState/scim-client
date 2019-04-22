2838fe59fa01d9fb4a394b52d2f6de7ab275e5ac
Steve Moyer <smoyer@psu.edu>
2019-04-22 14:37:26 -0400

    Updated to properly embody a custom SCIM resource

--- _examples/organization.go
+++ _examples/organization.go
@@ -7,7 +7,7 @@ import "github.com/PennState/golang_scimclient/scim"
 //as to zero or more child organizations.  As with other SCIM references
 //the Parent and Children references may be absolute or relative URIs.
 type Organization struct {
-	scim.Resource
+	scim.CommonAttributes
 	Name     string                `json:"name"`      //Name is the organization's name - e.g. "Tour Promotion"
 	Type     string                `json:"type"`      //Type is the organization's type - e.g. "Department"
 	Parent   OrganizationReference `json:"$parent"`   //Parent is a URI reference to a parent organization
@@ -17,3 +17,42 @@ type Organization struct {
 //OrganizationReference is a string containing an absolute or relative
 //URL to another Organization.
 type OrganizationReference string
+
+const OrganizationURN = "urn:com:example:2.0:Organization"
+
+//OrganizationResourceType provides the default structure which connects the
+//Organization struct to its associated ResourceType.
+var OrganizationResourceType = scim.ResourceType{
+	CommonAttributes: scim.CommonAttributes{
+		Schemas: []string{
+			scim.ResourceTypeURN,
+		},
+		ID: "Organization",
+	},
+	Name:        "Organization",
+	Endpoint:    "/Organizations",
+	Description: "SCIM ResourceType - See https://tools.ietf.org/html/rfc7643#section-6",
+	Schema:      scim.ResourceTypeURN,
+}
+
+//URN returns the IANA registered SCIM name for the Organization data structure
+//and, together with ResourceType() identifies this code as implementing
+//the Resource interface.
+func (o Organization) URN() string {
+	return OrganizationURN
+}
+
+//ResourceType returns the default structure describing the availability
+//of the Organization resource and, together with URN() identifies this code as
+//implementing the Resource interface.
+func (o Organization) ResourceType() scim.ResourceType {
+	return OrganizationResourceType
+}
+
+func (o *Organization) MarshalJSON() ([]byte, error) {
+	return scim.Marshal(o)
+}
+
+func (o *Organization) UnmarshalJSON(json []byte) error {
+	return scim.Unmarshal(json, o)
+}
