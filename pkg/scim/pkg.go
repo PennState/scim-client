ee97b3a8eb0539b4a2370541aeae6c013d57e22b
Steve Moyer <smoyer@psu.edu>
2019-12-06 11:53:15 -0500

    Fixes typo

--- pkg/scim/pkg.go
+++ pkg/scim/pkg.go
@@ -1,5 +1,5 @@
 /*
-Package go provides a client for "System for Cross-domain Identity
+Package scim provides a client for "System for Cross-domain Identity
 Management" (SCIM) servers as defined by RFC7642, RFC7643 and RFC7644.
 More information on the SCIM schemas and protocol can be found at
 http://www.simplecloud.info.
