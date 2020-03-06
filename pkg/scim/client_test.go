a8793edafddd07d2c1573636d5d33f42da3197f1
Steve Moyer <smoyer@psu.edu>
2020-03-06 15:53:32 -0500

    Adds tests for ClientOpt (methods and parsing)

--- pkg/scim/client_test.go
+++ pkg/scim/client_test.go
@@ -1,10 +1,37 @@
 package scim
 
 import (
+	"fmt"
+	"math"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+func TestClientOptsParsing(t *testing.T) {
+	type booleanOpt func(bool) ClientOpt
+	opts := []booleanOpt{DisableDiscovery, DisableEtag, IgnoreRedirects}
+	count := int(math.Pow(2, float64(len(opts))))
+	for i := 0; i < count; i++ {
+		dd := i&1 != 0
+		de := i&2 != 0
+		ir := i&4 != 0
+		name := fmt.Sprintf("DisableDiscovery: %t, DisableEtag: %t, IgnoreRedirects: %t", dd, de, ir)
+		t.Run(name, func(t *testing.T) {
+			c, err := NewClient(
+				nil,
+				"https://example.com/scim",
+				DisableDiscovery(dd),
+				DisableEtag(de),
+				IgnoreRedirects(ir),
+			)
+			assert.NoError(t, err)
+			assert.Equal(t, dd, c.cfg.DisableDiscovery)
+			assert.Equal(t, de, c.cfg.DisableEtag)
+			assert.Equal(t, ir, c.cfg.IgnoreRedirects)
+		})
+	}
+}
 
 func TestServiceURLParsing(t *testing.T) {
 	tests := []struct {
