fb493ea1a16ed2662cdf4fb53ac5163ed44df5e7
mez5001 <mez5001@psu.edu>
2022-08-25 14:36:22 -0400

    Adding Typed and Key itnerface. Extract extract generic functions

--- go.mod
+++ go.mod
@@ -1,15 +1,28 @@
 module github.com/PennState/scim-client
 
-go 1.13
+go 1.18
 
 require (
 	github.com/PennState/additional-properties v0.10.0
 	github.com/PennState/httputil v0.0.0-20200331141511-b6cd879122fd
 	github.com/PennState/proctor v0.3.0
-	github.com/json-iterator/go v1.1.8 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/onrik/logrus v0.4.1
 	github.com/sirupsen/logrus v1.4.2
 	github.com/stretchr/testify v1.5.1
 	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/golang/protobuf v1.2.0 // indirect
+	github.com/json-iterator/go v1.1.8 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859 // indirect
+	golang.org/x/sys v0.0.0-20191025090151-53bf42e6b339 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- pkg/scim/resource.go
+++ pkg/scim/resource.go
@@ -9,14 +9,24 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-//Named identifies the implementing code as including a SCIM URN
+// Named identifies the implementing code as including a SCIM URN
 type Named interface {
 	URN() string
 }
 
-//ServerDiscoveryResource identifies the implementing code as a SCIM
-//service discovery resource (Schemas, ServiceProviderConfig and
-//ResourceTypes).
+// Structs that have a Type field
+type Typed interface {
+	GetType() string
+}
+
+// Structs that have a Type field
+type Keyed interface {
+	GetKey() string
+}
+
+// ServerDiscoveryResource identifies the implementing code as a SCIM
+// service discovery resource (Schemas, ServiceProviderConfig and
+// ResourceTypes).
 type ServerDiscoveryResource interface {
 	Named
 	ResourceType() ResourceType
@@ -29,20 +39,20 @@ type resource interface {
 	getMeta() *Meta
 }
 
-//Resource identifies the implementing code as a SCIM resource.  Resources
-//defined by the specfication are User and Group.
-//https://tools.ietf.org/html/rfc7643#section-3
+// Resource identifies the implementing code as a SCIM resource.  Resources
+// defined by the specfication are User and Group.
+// https://tools.ietf.org/html/rfc7643#section-3
 type Resource interface {
 	resource
 	ServerDiscoveryResource
 }
 
-//Extension is the SCIM method for adapting and extending Resources
-//https://tools.ietf.org/html/rfc7643#section-3.3
+// Extension is the SCIM method for adapting and extending Resources
+// https://tools.ietf.org/html/rfc7643#section-3.3
 type Extension Named
 
-//CommonAttributes describes the base common attributes of all Scim Resources
-//https://tools.ietf.org/html/rfc7643#section-3.1
+// CommonAttributes describes the base common attributes of all Scim Resources
+// https://tools.ietf.org/html/rfc7643#section-3.1
 type CommonAttributes struct {
 	ID                   string                     `json:"id"`
 	ExternalID           string                     `json:"externalId,omitempty"`
@@ -51,8 +61,8 @@ type CommonAttributes struct {
 	AdditionalProperties map[string]json.RawMessage `json:"*"`
 }
 
-//Meta is a complex attribute containing resource metadata.
-//https://tools.ietf.org/html/rfc7643#section-3.1
+// Meta is a complex attribute containing resource metadata.
+// https://tools.ietf.org/html/rfc7643#section-3.1
 type Meta struct {
 	ResourceType string    `json:"resourceType"`
 	Created      time.Time `json:"created"`
@@ -61,19 +71,56 @@ type Meta struct {
 	Version      string    `json:"version"`
 }
 
-//Multivalued attributes contain a list of elements using the JSON array format defined in Section 5 of [RFC7159].
-//https://tools.ietf.org/html/rfc7643#section-2.4
+// Multivalued attributes contain a list of elements using the JSON array format defined in Section 5 of [RFC7159].
+// https://tools.ietf.org/html/rfc7643#section-2.4
 type Multivalued struct {
+	Key       string `json:"key,omitempty"`     // hack to align with PSU/Apache SCIMple
 	Type      string `json:"type,omitempty"`    //Type is a label indicating the attribute's function; e.g., 'work' or 'home'.
 	Display   string `json:"display,omitempty"` //Display is a  human readable name, primarily used for display purposes. READ-ONLY.
 	Primary   bool   `json:"primary,omitempty"` //Primary is a boolean value indicating the 'primary' or preferred attribute value for this attribute, e.g. the preferred mailing address or primary e-mail address. The primary attribute value 'true' MUST appear no more than once.
 	Reference string `json:"$ref,omitempty"`    //Reference is the reference URI of a target resource, if the attribute is a reference.
 }
 
-//StringMultivalued provides a base structure for simple string multi-valued attributes.
+// StringMultivalued provides a base structure for simple string multi-valued attributes.
 type StringMultivalued struct {
 	Multivalued
 	Value string `json:"value"` //The attribute's significant value, e.g., email address, phone	numbeca.
+}
+
+func (m Multivalued) GetType() string {
+	return m.Type
+}
+
+func (m Multivalued) GetKey() string {
+	return m.Key
+}
+
+func ExtractByType[T Typed](values []T, selector string) *T {
+	for _, v := range values {
+		if strings.EqualFold(v.GetType(), selector) {
+			return &v
+		}
+	}
+	return nil
+}
+
+func ExtractFirstMatchingType[T Typed](values []T, selectors ...string) *T {
+	for _, selector := range selectors {
+		value := ExtractByType(values, selector)
+		if value != nil {
+			return value
+		}
+	}
+	return nil
+}
+
+func ExtractByKey[T Keyed](values []T, key string) *T {
+	for _, v := range values {
+		if strings.EqualFold(v.GetKey(), key) {
+			return &v
+		}
+	}
+	return nil
 }
 
 //
@@ -97,9 +144,9 @@ func (ca CommonAttributes) getMeta() *Meta {
 	return &ca.Meta
 }
 
-//AddExtension adds a new SCIM extension to a SCIM resource.  This method is
-//purposely designed to return an error if the provided extension's URN is
-//already a key in the additionalProperties map.
+// AddExtension adds a new SCIM extension to a SCIM resource.  This method is
+// purposely designed to return an error if the provided extension's URN is
+// already a key in the additionalProperties map.
 func (ca *CommonAttributes) AddExtension(extension Extension) error {
 	if ca.HasExtension(extension) {
 		return errors.New("Extension to be added already exists in resource - use UpdateExtension() instead")
@@ -108,17 +155,17 @@ func (ca *CommonAttributes) AddExtension(extension Extension) error {
 	return err
 }
 
-//GetExtension retrieves a SCIM Extension from the additionalProperties map
-//by the Extension's URN.
+// GetExtension retrieves a SCIM Extension from the additionalProperties map
+// by the Extension's URN.
 func (ca *CommonAttributes) GetExtension(extension Extension) error {
 	name := extension.URN()
 	err := json.Unmarshal(ca.AdditionalProperties[name], extension)
 	return err
 }
 
-//GetExtensionURNs returns a list of the keys in the additionalProperties
-//map that start with "urn:".  Clearly this is not a perfect way to
-//guarantee that the RawMessage stored in that key is an extension.
+// GetExtensionURNs returns a list of the keys in the additionalProperties
+// map that start with "urn:".  Clearly this is not a perfect way to
+// guarantee that the RawMessage stored in that key is an extension.
 func (ca *CommonAttributes) GetExtensionURNs() []string {
 	keys := make([]string, 0, len(ca.AdditionalProperties))
 	for key := range ca.AdditionalProperties {
@@ -131,15 +178,15 @@ func (ca *CommonAttributes) GetExtensionURNs() []string {
 	return keys
 }
 
-//HasExtension indicates whether the URN included with the passed
-//Extension is a key in the additionalProperties map.
+// HasExtension indicates whether the URN included with the passed
+// Extension is a key in the additionalProperties map.
 func (ca *CommonAttributes) HasExtension(extension Extension) bool {
 	urn := extension.URN()
 	return ca.HasExtensionByURN(urn)
 }
 
-//HasExtensionByURN indicates whether the passed URN string is a key in
-//the additionalProperties map.
+// HasExtensionByURN indicates whether the passed URN string is a key in
+// the additionalProperties map.
 func (ca *CommonAttributes) HasExtensionByURN(urn string) bool {
 	_, exists := ca.AdditionalProperties[urn]
 	return exists
@@ -162,21 +209,21 @@ func (ca *CommonAttributes) putExtension(extension Extension) error {
 	return nil
 }
 
-//RemoveExtension deletes the RawMessage with the URN included with the
-//passed SCIM Extension from the additionalProperties map.
+// RemoveExtension deletes the RawMessage with the URN included with the
+// passed SCIM Extension from the additionalProperties map.
 func (ca *CommonAttributes) RemoveExtension(extension Extension) {
 	ca.RemoveExtensionByURN(extension.URN())
 }
 
-//RemoveExtensionByURN deletes the RawMessage with the key matching
-//the passed URN from the additionalProperties map.
+// RemoveExtensionByURN deletes the RawMessage with the key matching
+// the passed URN from the additionalProperties map.
 func (ca *CommonAttributes) RemoveExtensionByURN(urn string) {
 	delete(ca.AdditionalProperties, urn)
 }
 
-//UpdateExtension changes an existing SCIM extension already stored in a SCIM
-//resource.  This method is purposely designed to return an error if the
-//provided extension's URN is not a key in the additionalProperties map.
+// UpdateExtension changes an existing SCIM extension already stored in a SCIM
+// resource.  This method is purposely designed to return an error if the
+// provided extension's URN is not a key in the additionalProperties map.
 func (ca *CommonAttributes) UpdateExtension(extension Extension) error {
 	if !ca.HasExtension(extension) {
 		return errors.New("Extension to be updated does not exist in resource - use AddExtension() instead")
@@ -193,7 +240,7 @@ func (ca *CommonAttributes) UpdateExtension(extension Extension) error {
 //
 //
 
-//UnmarshalJSON turns the meta string values on the wire into the correct types
+// UnmarshalJSON turns the meta string values on the wire into the correct types
 func (m *Meta) UnmarshalJSON(j []byte) error {
 	var rawStrings map[string]string
 
@@ -207,22 +254,16 @@ func (m *Meta) UnmarshalJSON(j []byte) error {
 		if key == "resourcetype" {
 			m.ResourceType = v
 		} else if key == "created" {
-			t, err := time.Parse(time.RFC3339, v)
-			if err != nil {
-				value := fixTimeZone(v)
-				t, err = time.Parse(time.RFC3339, value)
-			}
+			value := fixTimeZone(v)
+			t, err := time.Parse(time.RFC3339, value)
 
 			if err != nil {
 				return err
 			}
 			m.Created = t
 		} else if key == "lastmodified" {
-			t, err := time.Parse(time.RFC3339, v)
-			if err != nil {
-				value := fixTimeZone(v)
-				t, err = time.Parse(time.RFC3339, value)
-			}
+			value := fixTimeZone(v)
+			t, err := time.Parse(time.RFC3339, value)
 
 			if err != nil {
 				return err
